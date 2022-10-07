c5749b48abf4fe49b3e5a9f51a7e406c6445dc6b
ChandanaJ16 <86908712+ChandanaJ16@users.noreply.github.com>
2022-10-07 17:26:43 +0530

    added input.m
    Demonstrates usage of input() to read the data from user

---
+++ input.m
@@ -0,0 +1,5 @@
+%program demonstrating usage of input() to read the data from user
+x=input('Enter number 1: ');
+y=input('Enter number 2: ');
+m=x*y;
+fprintf('Multiplication of a and b is: %.2f',m);
