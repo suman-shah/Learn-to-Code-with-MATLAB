a15e8e5c2eacce6ec9c4409c7d339d50e0bdabc4
vanshika <vanshikabansal73@gmail.com>
2023-10-15 17:37:55 +0530

    whileloop

---
+++ WhileLoop.m
@@ -0,0 +1,10 @@
+% While loop is a loop which runs until its condition becomes false.
+% For example: lets take a variable and print its value until it is less than 20.
+
+
+a = 10; %initialisation of variable
+ % while loop execution 
+while( a < 20 ) %syntax for while loop
+ fprintf('value of a: %d\n', a); %printing value
+ a = a + 1; %updating the value of variable within loop
+end  %terminating the loop
